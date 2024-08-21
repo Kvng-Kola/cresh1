8590daa37339e9c4186c5805371d178815acf884
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2024-08-21 12:11:07 +0100

    validating student registration form

--- student_registration.php
+++ student_registration.php
@@ -1,13 +1,15 @@
 <?php
     $pageTitle = "Student Registration";
     require_once("assets/header.php");
-
+   
     // Initializing Variables
-    $passError = "";
-    $firstname = $middlename = $surname = $gender = $dob = $password = $cpassword = $student_address = $class = $department = "";
+    $passError = $phone1Error = $phone2Error = ""; # Error Variables
+    $firstname = $middlename = $surname = $gender = $dob = $password = $cpassword = $student_address = $class = $department = ""; // Student Variables
+    $g_firstname = $g_middlename = $g_surname = $g_gender = $g_email = $g_phone1 = $g_phone2 = $g_maritalstatus = $guardian_address = ""; // Guardian Variables
 
     // Form Validation
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Capturing student bio-data
         $firstname = $_POST["firstname"];
         $middlename = $_POST["middlename"];
         $surname = $_POST["surname"];
@@ -19,24 +21,59 @@
         $class = $_POST['class'];
         $department = $_POST['department'];
 
+        // Capturing guardian bio-data
+        $g_firstname = $_POST["g_firstname"];
+        $g_middlename = $_POST["g_middlename"];
+        $g_surname = $_POST["g_surname"];
+        $g_gender = $_POST["g_gender"];
+        $g_email = $_POST["g_email"];
+        $g_phone1 = $_POST["g_phone1"];
+        $g_phone2 = $_POST["g_phone2"];
+        $g_maritalstatus = $_POST["g_maritalstatus"];
+        $guardian_address = $_POST['guardian_address'];
+
         // Password Validation
         if($password == $cpassword) {
             if(preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@%$&?-_])[A-Za-z\d!@%$&?-_]{8,}/", $password)) {
                 $pass = password_hash($password, PASSWORD_DEFAULT);
-                echo "$firstname, $middlename, $surname, $gender, $dob, $pass, $cpassword, $class, $department, $student_address";
             } else {
                 $passError = "Password must contain at least 8 characters, including uppercase, lowercase, number, and special character";
             }
         } else {
             $passError = "Password do not match";
         }
+
+        // Phone Number Validation
+        if (!preg_match('/^0[789][01]\d{8}$|^\+234[789][01]\d{8}$/', $g_phone1)) {
+            $phone1Error = "Invalid Phone Number";
+        }
+
+        if (!empty($g_phone2)) {
+            if (!preg_match('/^0[789][01]\d{8}$|^\+234[789][01]\d{8}$/', $g_phone2)) {
+                $phone2Error = "Invalid Phone Number";
+            }
+        } 
+
+        // Capturing Guardian Address
+        if(empty($guardian_address)) {
+            $guardian_address = $student_address;
+        }
+
+
+        // Population the database
+        if($passError == "" && $phone1Error == "" && $phone2Error =="") {
+            echo "$firstname, $middlename, $surname, $gender, $dob, $pass, $cpassword, $class, $department, $student_address";
+        } else {
+            echo "Errors: $passError, $phone1Error, $phone2Error";
+        }
         
     }
 
 ?>
 
-<main class="m-5 p-5 bg-secondary">
-    <form autocomplete="off" method="post" action="">
+<main class="m-5 p-5" style="background: gray;">
+    <form autocomplete="off" method="post" action="" style="display: flex; gap: 20px; flex-flow: column">
+        <h1>Student Bio Data</h1>
         <div class="row">
             <div class="col">
                 <input type="text" class="form-control" placeholder="First name" name="firstname" value="<?= $firstname?>" required/>
@@ -70,14 +107,14 @@
                 <span class="text-danger"><?= $passError?></span>
             </div>
             <div class="col">
-                <input type="password" class="form-control" placeholder="Confirm Password" name="cpassword" value="<?= $cpassword?>" required/>
+                <input type="password" class="form-control" placeholder="Confirm Password" name="cpassword" value="<?= $cpassword ?>" required />
                 <span class="text-danger"><?= $passError?></span>
             </div>
         </div>
 
         <div class="row">
             <div class="col">
-                <textarea placeholder="Home Address" name="student_address" class="form-control"><?= $student_address?></textarea>
+                <textarea placeholder="Home Address" name="student_address" class="form-control" required><?= $student_address?></textarea>
             </div>
         </div>
 
@@ -115,9 +152,64 @@
                 </select>
             </div>
         </div>
+
+        <h1>Guardian Biodata</h1>
+
         <div class="row">
             <div class="col">
-                <input type="submit" value="Register Student" class="form-control"/>
+                <input type="text" class="form-control" placeholder="First name" name="g_firstname" value="<?= $g_firstname?>" required/>
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Middlename (Optional)" name="g_middlename" value="<?= $g_middlename?>"/>
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Surname" name="g_surname" value="<?= $g_surname?>" required/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <input type="tel" class="form-control" placeholder="Phone Number" name="g_phone1" value="<?= $g_phone1?>" required/>
+                <span class="text-danger"><?= $phone1Error ?></span>
+            </div>
+            <div class="col">
+                <input type="tel" class="form-control" placeholder="Alt Phone Number (Optional)" name="g_phone2" value="<?= $g_phone2?>"/>
+                <span class="text-danger"><?= $phone2Error ?></span>
+            </div>
+            <div class="col">
+                <input type="email" class="form-control" placeholder="E-Mail Address" name="g_email" value="<?= $g_email?>" required/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Gender:
+                <input type="radio" name="g_gender" value="Male"/> Male
+                <input type="radio" name="g_gender" value="Female" checked/> Female
+                <input type="radio" name="g_gender" value="Others"/> Others
+            </div>
+            <div class="col">
+                <div class="d-flex">
+                    Marital Status:
+                    <select name="g_maritalstatus" required class="form-control" value="<?=$g_maritalstatus?>">
+                        <option value="Single">Single</option>
+                        <option value="Married">Married</option>
+                        <option value="Divorced">Divorced</option>
+                        <option value="Seperated">Seperated</option>
+                        <option value="Widowed">Widowed</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <textarea placeholder="Home Address" name="guardian_address" class="form-control"><?= $guardian_address?></textarea>
+            </div>
+        </div>
+
+        <div class="row ">
+            <div class="col">
+                <input type="submit" value="Register Student" class="form-control btn btn-primary"/>
             </div>
         </div>
     </form>
